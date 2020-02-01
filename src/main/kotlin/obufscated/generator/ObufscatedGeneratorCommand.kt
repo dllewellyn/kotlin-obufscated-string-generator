173a36dc4b0371d57269dad2166b439555fc48c2
Daniel Llewellyn <daniel.llewellyn@xdesign.com>
2020-02-01 13:41:59 +0000

    Initial commit

---
+++ src/main/kotlin/obufscated/generator/ObufscatedGeneratorCommand.kt
@@ -0,0 +1,44 @@
+package obufscated.generator
+
+import com.secure.obfuscated.xorObfuscator
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+import java.security.SecureRandom
+
+@Command(name = "obufscated-generator", description = ["..."],
+        mixinStandardHelpOptions = true)
+class ObufscatedGeneratorCommand : Runnable {
+
+    @Option(names = ["-k", "--key"], description = ["Obfucsation key to use"], required = false)
+    private var key: String? = null
+
+    @Option(names = ["-v", "--value"], description = ["Value for obfuscation"], required = true)
+    private var value: String? = null
+    private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+    override fun run() {
+        val keyTouse = if (key == null) {
+            (1..value!!.length)
+                    .map { i -> kotlin.random.Random.nextInt(0, charPool.size) }
+                    .map(charPool::get)
+                    .joinToString("");
+        } else {
+            key!!
+        }
+
+        println("Key: $keyTouse Obfuscated string ${xorObfuscator(keyTouse, value!!)}")
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            PicocliRunner.run(ObufscatedGeneratorCommand::class.java, *args)
+        }
+    }
+}
